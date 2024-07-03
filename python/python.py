a1ec65a1b58d786219a4696d25b61fad1c3e719e
oAmorim2k5 <victorhuski@gmail.com>
2024-07-03 09:05:32 -0300

    Segundo Commit sobre API

--- python/python.py
+++ python/python.py
@@ -1,34 +1,47 @@
 from imports import *
+import time
 
-option = int(input("""
-=======MENU=======
-1 - Dolar / Real
-2 - Euro / Real
-3 - Bit / Real
-                   
-0 - Sair
-==================
-"""))
+lauching = bool(True)
+while lauching == True:
+    option = int(input("""
+    =======MENU=======
+    1 - Dolar / Real
+    2 - Euro / Real
+    3 - Bit / Real
+                    
+    0 - Sair
+    ==================
+    Selecionar: 
+    """))
 
-match option:
-    case 1:
-        os.system('cls')
-        cotacoes_dolar = float(cotacoes_dolar)
-        entry = float(input("======Dolar para Real======\nValor em Real: R$"))
-        print("Cotação do Dolar: {:.2f}".format(cotacoes_dolar))
-        entry = entry* float(cotacoes_dolar)
-        print("Real para Dolar: R$ {:.2f}".format(entry))
-    case 2:
-        os.system('cls')
-        cotacoes_euro = float(cotacoes_euro)
-        entry = float(input("======Euro para Real======\nValor em Real: R$"))
-        print("Cotação do Euro: {:.2f}".format(cotacoes_euro))
-        entry = entry* float(cotacoes_euro)
-        print("Real para Euro: R$ {:.2f}".format(entry))
-    case 3:
-        os.system('cls')
-        cotacoes_bit = float(cotacoes_bit)
-        entry = float(input("======Bit-Coin para Real======\nValor em Real: R$"))
-        print("Cotação do Bit: {:.2f}".format(cotacoes_bit))
-        entry = entry* float(cotacoes_bit)
-        print("Real para Bit-coin: R$ {:.2f}".format(entry))
+    match option:
+        case 1:
+            os.system('cls')
+            cotacoes_dolar = float(cotacoes_dolar)
+            entry = float(input("======Dolar para Real======\nValor em Real: R$"))
+            print("Cotação do Dolar: {:.2f}".format(cotacoes_dolar))
+            entry = entry* float(cotacoes_dolar)
+            print("Real para Dolar: R$ {:.2f}".format(entry))
+        case 2:
+            os.system('cls')
+            cotacoes_euro = float(cotacoes_euro)
+            entry = float(input("======Euro para Real======\nValor em Real: R$"))
+            print("Cotação do Euro: {:.2f}".format(cotacoes_euro))
+            entry = entry* float(cotacoes_euro)
+            print("Real para Euro: R$ {:.2f}".format(entry))
+        case 3:
+            os.system('cls')
+            cotacoes_bit = float(cotacoes_bit)
+            entry = float(input("======Bit-Coin para Real======\nValor em Real: R$"))
+            print("Cotação do Bit: {:.2f}".format(cotacoes_bit))
+            entry = entry* float(cotacoes_bit)
+            print("Real para Bit-coin: R$ {:.2f}".format(entry))
+        case 0:
+            i=3
+            print("Encerrando sistema...")
+            while i != 0:
+                time.sleep(0.6)
+                print(f"{i}...")
+                i-=1
+            print()
+            lauching = bool(False)
---
+++ python/python_def.py
@@ -0,0 +1,80 @@
+from imports import *
+import time
+
+def main():
+    lauching = bool(True)
+    try:
+        option = int(input("""
+        -=======MENU=======-
+         |1 - Dolar / Real|
+         |2 - Euro / Real |
+         |3 - Bit / Real  |
+         |                |
+         |0 - Sair        |
+        -==================-
+        ---Selecionar: """))
+
+        if option == 1:
+            convert_dolar()
+        elif option == 2:
+            convert_euro()
+        elif option == 3:
+            convert_bit()
+        elif option == 0:
+            sair()
+    except:
+        print("Opção inexistente ou formato errado, verifique seu valor de entrada e tente novamente!!!")
+        time.sleep(2)
+        os.system('cls')
+        main()
+
+def convert_dolar():
+    try:
+        os.system('cls')
+        entry = float(input("======Dolar para Real======\nValor em Real: R$"))
+        print("Cotação do Dolar: {:.2f}".format(float(cotacoes_dolar)))
+        entry = entry* float(cotacoes_dolar)
+        print("Real para Dolar: R$ {:.2f}".format(entry))
+        time.sleep(2)
+    except:
+        print("Valor inválido verifique sua entrada e tente novamente")
+        time.sleep(1.5)
+        convert_dolar()
+
+def convert_euro():
+    try:
+        os.system('cls')
+        entry = float(input("======Euro para Real======\nValor em Real: R$"))
+        print("Cotação do Euro: {:.2f}".format(float(cotacoes_euro)))
+        entry = entry* float(cotacoes_euro)
+        print("Real para Euro: R$ {:.2f}".format(entry))
+        time.sleep(2)
+    except:
+        print("Valor inválido verifique sua entrada e tente novamente")
+        time.sleep(1.5)
+        convert_euro()
+
+def convert_bit():
+    try:
+        os.system('cls')
+        entry = float(input("======Bit-Coin para Real======\nValor em Real: R$"))
+        print("Cotação do Bit: {:.2f}".format(float(cotacoes_bit)))
+        entry = entry* float(cotacoes_bit)
+        print("Real para Bit-coin: R$ {:.2f}".format(entry))
+        time.sleep(2)
+    except:
+        print("Valor inválido verifique sua entrada e tente novamente")
+        time.sleep(1.5)
+        convert_bit()
+
+def sair():
+    i=3
+    print("Encerrando sistema...")
+    while i != 0:
+        time.sleep(0.6)
+        print(f"{i}...")
+        i-=1
+    lauching = bool(False)
+
+if __name__ == "__main__":
+    main()  
